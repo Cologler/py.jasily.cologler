8d295397e0385f3f8d926b4dcc85bbc889817d4d
赖巍 <skyoflw@gmail.com>
2016-11-08 15:04:13 +0800

    use unittest framework.

--- @example/check.py
+++ @example/check.py
@@ -6,6 +6,7 @@
 #
 # ----------
 
+import unittest
 import typing
 from jasily.exceptions import InvalidOperationException
 from jasily.check import check_arguments
@@ -47,38 +48,48 @@ else:
 def func_check(arg):
     return not isinstance(arg, str)
 
-@check_arguments
-def func3(arg1: func_check):
-    pass
+class TestCheckMethods(unittest.TestCase):
 
-# pass
-func3(None)
-func3(1)
-# error
-assert_error(TypeError, func3, '')
+    def test_check_arguments_3(self):
+        @check_arguments
+        def func(arg1: func_check):
+            pass
+        func(None)
+        func(1)
+        with self.assertRaises(TypeError):
+            func('')
 
-@check_return
-def func4(a) -> (str, None):
-    return a or 1
+    def test_check_arguments_4(self):
+        @check_arguments
+        def func(arg1, arg2: str=None):
+            pass
+        func(3)
 
-# pass
-func4('2')
-# error
-assert_error(TypeError, func4, None)
-assert_error(TypeError, func4, 1)
+    def test_check_return(self):
+        @check_return
+        def func(a) -> (str, None):
+            return a or 1
+        func('2')
+        with self.assertRaises(TypeError):
+            func(None)
+        with self.assertRaises(TypeError):
+            func(1)
 
-check_generic([], typing.List[int])
-check_generic([1], typing.List[int])
-assert_error(TypeError, check_generic, ['2'], typing.List[int])
+    def test_check_generic(self):
+        check_generic([], typing.List[int])
+        check_generic([1], typing.List[int])
+        check_generic({}, typing.Dict[int, str])
+        check_generic({1: ''}, typing.Dict[int, str])
+        check_generic((1, ''), typing.Tuple[int, str])
+        check_generic((1, ['']), typing.Tuple[int, typing.List[str]])
+        with self.assertRaises(TypeError):
+            check_generic(['2'], typing.List[int])
+        with self.assertRaises(TypeError):
+            check_generic({1: 2}, typing.Dict[int, str])
+        with self.assertRaises(TypeError):
+            check_generic({'': '2'}, typing.Dict[int, str])
+        with self.assertRaises(TypeError):
+            check_generic((1, ), typing.Tuple[int, str])
 
-check_generic({}, typing.Dict[int, str])
-check_generic({1: ''}, typing.Dict[int, str])
-assert_error(TypeError, check_generic, {1: 2}, typing.Dict[int, str])
-assert_error(TypeError, check_generic, {'': '2'}, typing.Dict[int, str])
-
-check_generic((1, ''), typing.Tuple[int, str])
-assert_error(TypeError, check_generic, (1, ), typing.Tuple[int, str])
-
-check_generic((1, ['']), typing.Tuple[int, typing.List[str]])
-
-print('test completed.')
+if __name__ == '__main__':
+    unittest.main()
