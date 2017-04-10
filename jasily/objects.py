4d23e0d9e03e7bbb728a669578e44d87a9f6cd3e
赖巍 <skyoflw@gmail.com>
2017-04-10 18:10:57 +0800

    moved guid.

--- jasily/objects.py
+++ jasily/objects.py
@@ -9,9 +9,6 @@
 import uuid
 from .exceptions import InvalidOperationException
 
-GUID_PY = 'py'
-GUID_NET = 'net'
-
 class __NotFound:
     '''a object for not found value.'''
     pass
@@ -48,45 +45,6 @@ class ValueContainer:
         self.__value = None
 
 
-class Guid:
-    def __init__(self, uid=None, mode=GUID_PY):
-        if uid is None:
-            uid = uuid.uuid4()
-        elif isinstance(uid, str):
-            uid = uuid.UUID(uid)
-        assert isinstance(uid, uuid.UUID)
-        assert mode in [GUID_PY, GUID_NET]
-        self._uuid = uid
-        self._mode = mode
-
-    def __str__(self):
-        return str(self._uuid)
-
-    def to_string(self):
-        '''str(self)'''
-        return str(self)
-
-    def convert_to_net(self):
-        '''convert to .net style guid.'''
-        if self._mode == GUID_NET:
-            return self
-        if self._mode == GUID_PY:
-            value = str(self)
-            converted = value[6:8] + value[4:6] + value[2:4] + value[0:2] +\
-                        value[8] + value[11:13] + value[9:11] + value[13] +\
-                        value[16:18] + value[14:16] + value[18:]
-            return Guid(converted)
-        raise NotImplementedError
-
-    def convert_to_py(self):
-        '''convert to python style guid.'''
-        if self._mode == GUID_PY:
-            return self
-        if self._mode == GUID_NET:
-            return Guid(self._uuid, GUID_PY).convert_to_net()
-        raise NotImplementedError
-
-
 __FREEZABLE_FLAG = '__JASILY_FREEZABLE_IS_FREEZED__'
 
 class Freezable:
@@ -106,10 +64,23 @@ class Freezable:
             raise InvalidOperationException('obj is freezed.')
 
 
+class UInt:
+    def __init__(self, value: int):
+        v = int(value)
+        if v < 0:
+            raise ValueError
+        self._value = v
+
+    @property
+    def value(self):
+        return self._value
+
+
 class Set(set):
     def add(self, v):
         if v in self:
             return False
         set.add(self, v)
         return True
+
 
