0a7d022b6763c8a63bd0c96d5f06d20ab339eaf0
赖巍 <skyoflw@gmail.com>
2017-04-21 21:01:12 +0800

    add customize hashable for dict.

--- jasily/objects.py
+++ jasily/objects.py
@@ -69,6 +69,105 @@ class UInt:
         return self._value
 
 
+class IComparer:
+    def hash(self, obj):
+        raise NotImplementedError
+
+    def eq(self, obj1, obj2):
+        raise NotImplementedError
+
+    def wrap(self, obj):
+        class Wrap:
+            def __init__(self, comparer, obj):
+                self._comparer = comparer
+                self._obj = obj
+
+            def unwrap(self):
+                return self._obj
+
+            def __repr__(self):
+                return self._obj.__repr__()
+
+            def __str__(self):
+                return self._obj.__str__()
+
+            def __hash__(self):
+                return self._comparer.hash(self._obj)
+
+            def __eq__(self, obj):
+                if isinstance(obj, Wrap):
+                    obj = obj.unwrap()
+                return self._comparer.eq(self._obj, obj)
+        return Wrap(self, obj)
+
+
+class ObjectComparer(IComparer):
+    def hash(self, obj):
+        return hash(obj)
+
+    def eq(self, obj1, obj2):
+        return obj1 == obj2
+
+
+class Dict(dict):
+    def __init__(self, comparer: IComparer=None, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+        self._comparer = comparer or ObjectComparer()
+
+    def __contains__(self, k):
+        k = self._comparer.wrap(k)
+        return super().__contains__(k)
+
+    def __delitem__(self, k):
+        k = self._comparer.wrap(k)
+        return super().__delitem__(k)
+
+    def __getitem__(self, k):
+        k = self._comparer.wrap(k)
+        return super().__getitem__(k)
+
+    def __iter__(self):
+        for k in super().__iter__():
+            yield k.unwrap()
+
+    def __setitem__(self, k, v):
+        k = self._comparer.wrap(k)
+        return super().__setitem__(k, v)
+
+    def fromkeys(self, *args):
+        if 0 < len(args):
+            def iterkeys(i):
+                for k in i:
+                    yield self._comparer.wrap(k)
+            args = list(args)
+            args[0] = iterkeys(args[0])
+        return super().fromkeys(*args)
+
+    def get(self, k, v):
+        k = self._comparer.wrap(k)
+        return super().get(k, v)
+
+    def keys(self):
+        return [k.unwrap() for k in super().keys()]
+
+    def pop(self, *args):
+        if 0 < len(args):
+            args = list(args)
+            args[0] = self._comparer.wrap(args[0])
+        return super().pop(*args)
+
+    def popitem(self):
+        k, v = super().popitem()
+        return k.unwrap(), v
+
+    def setdefault(self, k, d):
+        k = self._comparer.wrap(k)
+        return super().setdefault(k, d)
+
+    def update(self, *args, **kwargs):
+        raise NotImplementedError
+
+
 class Set(set):
     def add(self, v):
         size = len(self)
@@ -112,4 +211,3 @@ class Char:
             return int(self) == value
         else:
             return NotImplemented
-
