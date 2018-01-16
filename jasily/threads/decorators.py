e8b6aaa39fa9b2c898949356e2860a24c9e09716
Cologler <skyoflw@gmail.com>
2018-01-17 05:50:21 +0800

    add semaphore

--- jasily/threads/decorators.py
+++ jasily/threads/decorators.py
@@ -9,9 +9,8 @@
 import functools
 import threading
 
-def lock(func):
-    ''' use `Lock` to keep func access thread-safety. '''
-    sync = threading.Lock()
+def _create_wrapper(func, factory, factory_args=[], factory_kwargs={}):
+    sync = factory(*factory_args, **factory_kwargs)
 
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
@@ -19,14 +18,50 @@ def lock(func):
             return func(*args, **kwargs)
 
     return wrapper
+
+def lock(func):
+    '''
+    use `Lock` to keep func access thread-safety.
+
+    example:
+
+    ``` py
+    @lock
+    def func(): pass
+    ```
+    '''
+    return _create_wrapper(func, threading.Lock)
 
 def rlock(func):
-    ''' use `RLock` to keep func access thread-safety. '''
-    sync = threading.RLock()
+    '''
+    use `RLock` to keep func access thread-safety.
 
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        with sync:
-            return func(*args, **kwargs)
+    example:
 
-    return wrapper
+    ``` py
+    @rlock
+    def func(): pass
+    ```
+    '''
+    return _create_wrapper(func, threading.RLock)
+
+def semaphore(count: int, bounded: bool=False):
+    '''
+    use `Semaphore` to keep func access thread-safety.
+
+    example:
+
+    ``` py
+    @semaphore(3)
+    def func(): pass
+    ```
+    '''
+
+    lock_type = threading.BoundedSemaphore if bounded else threading.Semaphore
+
+    def _wrap(func):
+        return _create_wrapper(func, lock_type, factory_kwargs={'value': count})
+
+    return _wrap
+
+
