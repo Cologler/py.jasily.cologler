79e4164f4c66cd4281d14908cb6cee6167f01ad5
Cologler <skyoflw@gmail.com>
2018-01-18 02:06:49 +0800

    refactor switch api usage

--- jasily/lang/switch.py
+++ jasily/lang/switch.py
@@ -6,47 +6,58 @@
 #
 # ----------
 
-# pylint: disable=C0111
-def ensure_case(args):
-    if not args:
-        raise SyntaxError('cannot case empty pattern.')
-
 # pylint: disable=C0103
 # idea from https://code.activestate.com/recipes/410692/
 class switch(object):
     """
     to use `switch`, here are some examples:
 
-    **example 1:**
-
     ``` py
+    # example 1:
     for case in switch(value):
-        if case('A'):
-            pass
-        elif case(1, 3):
-            pass # for mulit-match.
+        if case('A', 1):
+            pass # value match any of ('A', 1)
         else:
             pass # for default.
-    ```
 
-    **example 2:**
+    # example 2:
+    with switch(value) as case:
+        if case('A', 1):
+            pass # value match any of ('A', 1)
+        else:
+            pass # for default.
 
-    ``` py
-    ret = switch(2).case('A', 1).case('B', 2, 4).default(3).end()
+    # example 3:
+    ret = switch(2).case('A', 1).case('B', 2, 4).default(3)
     assert ret == 'B'
     ```
 
     """
     def __init__(self, value):
         self._value = value
-        self._exec = False
 
     def __iter__(self):
-        """Return the match method once, then stop"""
+        """ return the match method once for `for` stat. """
         yield self.match
+
+    def __enter__(self):
+        """ return the match method for `with` stat. """
+        return self.match
+
+    def __exit__(self, *args):
+        pass
+
+    @staticmethod
+    def match_args(value, args):
+        '''check whether the value match the pattern args.'''
+        return value in args
 
     def match(self, *args):
         """
+        Indicate whether or not to enter a case suite.
+
+        usage:
+
         ``` py
         for case in switch(value):
             if case('A'):
@@ -57,47 +68,45 @@ class switch(object):
                 pass # for default.
         ```
         """
-        ensure_case(args)
-        if self._exec: # ready exec
-            raise SyntaxError('cannot call match() after matched.')
-        self._exec = self._value in args
-        return self._exec
+        if not args:
+            raise SyntaxError('cannot case empty pattern.')
+
+        return self.match_args(self._value, args)
 
     def case(self, value, *args):
-        ensure_case(args)
+        if not args:
+            raise SyntaxError('cannot case empty pattern.')
+
         test_value = self._value
 
-        class Default:
-            def __init__(self, has_value: bool, value):
-                self._has_value = has_value
-                self._value = value
+        class ICase:
+            def default(self, value):
+                return value
 
-            def end(self):
-                '''End the switcher.'''
-                return self._value
+        class Case(ICase):
 
-        class Case(Default):
-            def __init__(self, has_value: bool, value):
-                '''
-                args mode:
+            def case(self, value, *args):
+                if not args:
+                    raise SyntaxError('cannot case empty pattern.')
 
-                if has_value is True:
-                    value is finally value.
+                if switch.match_args(test_value, args):
+                    return ResultCase(value)
                 else:
+                    return CASE
 
-                '''
-                self._has_value = has_value
+        CASE = Case()
+
+        class ResultCase(ICase):
+            def __init__(self, value):
                 self._value = value
 
             def case(self, value, *args):
-                ensure_case(args)
-                if self._has_value:
-                    return self
-                else:
-                    return Case(test_value in args, value)
+                if not args:
+                    raise SyntaxError('cannot case empty pattern.')
+
+                return self
 
             def default(self, value):
-                ret_val = self._value if self._has_value else value
-                return Default(True, ret_val)
+                return self._value
 
-        return Case(test_value in args, value)
+        return CASE.case(value, *args)
