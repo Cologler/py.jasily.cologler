b99e37b15f26eeb2fb734952bb15d53b3b63bc7f
Cologler <skyoflw@gmail.com>
2018-01-22 13:31:49 +0800

    add readonly proxy

---
+++ @test/lang/test_readonlys.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017~2999 - cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+import os
+import sys
+import traceback
+import unittest
+
+from jasily.lang.readonlys import readonly
+
+
+class Test(unittest.TestCase):
+    def test_readonly(self):
+        class A:
+            pass
+
+        for error_on_set in (True, False):
+            a = A()
+            roa = readonly(a, error_on_set=error_on_set)
+
+            self.assertIsInstance(roa, type(a))
+
+            with self.assertRaises(AttributeError):
+                _ = roa.b
+            if error_on_set:
+                with self.assertRaises(AttributeError):
+                    roa.b = 1
+            else:
+                roa.b = 1
+            with self.assertRaises(AttributeError):
+                _ = roa.b
+
+            a.b = 'b'
+            self.assertEqual('b', roa.b)
+            if error_on_set:
+                with self.assertRaises(AttributeError):
+                    roa.b = 'b'
+            else:
+                roa.b = 'b'
+            self.assertEqual('b', roa.b)
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    try:
+        unittest.main()
+    except Exception:
+        traceback.print_exc()
+
+if __name__ == '__main__':
+    main()
---
+++ jasily/lang/readonlys.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017~2999 - cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+from functools import update_wrapper
+
+def readonly(obj, *, error_on_set = False):
+    '''
+    return readonly proxy for the `obj`.
+    '''
+    base_cls = type(obj)
+
+    class Readonly(base_cls):
+        def __getattribute__(self, name):
+            return getattr(obj, name)
+
+        def __setattr__(self, name, value):
+            if error_on_set:
+                raise AttributeError('cannot set readonly object.')
+
+    update_wrapper(Readonly, base_cls, updated = ())
+    return Readonly()
